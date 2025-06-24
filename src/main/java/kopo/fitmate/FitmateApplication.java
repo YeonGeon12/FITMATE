13edd7bdadd87ed34d878e099acc3d76731fcd56
윤건 <yungun200512@gmail.com>
2025-06-24 13:21:29 +0900

    외부 파일 업로드

---
+++ src/main/java/kopo/fitmate/FitmateApplication.java
@@ -0,0 +1,12 @@
+package kopo.fitmate; // 이 패키지 경로는 다를 수 있습니다.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmateApplication { // 파일 이름은 실제 프로젝트에 맞게 확인해주세요.
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitmateApplication.class, args);
+    }
+}
